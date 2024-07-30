d3fc3df63a61889c0abd1e798877c6db8cd9102f
Bart Massey <bart.massey@gmail.com>
2024-07-30 15:22:25 -0700

    finished motor lib; untested

--- src/motor.rs
+++ src/motor.rs
@@ -1,1 +1,24 @@
-//
+use crate::*;
+
+pub enum Motor {
+    M1,
+    M2,
+}
+
+impl<D, T> WuKong<D, T>
+where
+    D: delay::DelayNs,
+    T: twim::Instance,
+{
+    pub fn set_motor_speed(&mut self, motor: Motor, speed: i8) -> Result<(), twim::Error> {
+        let motor = match motor {
+            Motor::M1 => 1,
+            Motor::M2 => 2,
+        };
+        let sign = if speed >= 0 { 1 } else { 2 };
+        // XXX Fixme: Return error on overspeed.
+        let speed = speed.abs().min(100);
+        let buf = [motor, sign as u8, speed as u8, 0];
+        self.i2c.write(Self::I2C_ADDR, &buf)
+    }
+}
