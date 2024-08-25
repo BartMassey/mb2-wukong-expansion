6466da9e43c57bca434fb355e7439cea523ba95d
Bart Massey <bart.massey@gmail.com>
2024-08-24 22:55:45 -0700

    updated deps to use fixed ws2812 crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/mb2-wukong-expansion/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/mb2-wukong-expansion/0.1.2")]
 /*!
 
 This Rust crate provides support for the [Elecfreaks Wukong
